02004036a55c1760e1ffa4917dbd325a18626f99
ugoetudo <uoe12341234@gmail.com>
2024-09-19 18:30:46 -0400

    user check endpoint

---
+++ db.js
@@ -0,0 +1,8 @@
+const name = "db"
+const conn_info = {
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'practice_project_530'
+}
+module.exports = {name, conn_info}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
+const db = require('../db.js')
+const mysql = require('mysql')
 var express = require('express');
 var router = express.Router();
+var https = require("https");
+var cors = require("cors");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* npm install cors */
+/* to start mysql "..\..\Program Files\MySQL\MySQL Server 8.0\bin\mysql" -uroot -p  ==> this will prompt for a password which should be "password" at first*/
+/* mysql> status; ==> shows connection information which you'll need later */
+/* insert into student_user values (120, 'ugo', 'etudo', 'etudouo@vcu.edu', now()); */
+
+const connection = mysql.createConnection(db.conn_info)
+
+connection.connect()
+
+var corsOptions = {
+  origin: "*",
+}
+
+router.get('/', cors(corsOptions), function(req, res, next) {
+  const vnum = req.query.vnum
+  connection.query('select first_name, last_name from student_user where vnum = ?', [vnum], (e, r, f) => {
+    if (!e) {
+      res.json(r[0])
+    }
+    else {
+      next(e)
+    }
+  })
 });
 
 module.exports = router;
