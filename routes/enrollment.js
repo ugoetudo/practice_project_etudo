24901115ee57a84f2212f6e002a7a5e01be0711e
ugoetudo <uoe12341234@gmail.com>
2024-09-23 14:20:53 -0400

    correctly filter and display course data

--- routes/enrollment.js
+++ routes/enrollment.js
@@ -12,52 +12,68 @@ var corsOptions = {
   origin: "*",
 }
 
-const time_left = (result) => {
-    const tn = new Date()
-    const day_names = ['m', 't', 'w', 'tr', 'f']
-    const dtw = tn.getDay()
-    const yr = tn.getFullYear()
-    const dt = tn.getDate()
-    const mnt = tn.getMonth()
-    const dow = day_names[dtw - 1]
+const is_valid_course = (elem) => {
+    if (elem) {
+        return true
+    }
+    else {
+        return false
+    }
+}
 
+const time_left = (result) => {
+    var tn = new Date()
+    const day_names = ['m', 't', 'w', 'tr', 'f']
+    var dtw = tn.getDay()
+    var yr = tn.getFullYear()
+    var dt = tn.getDate()
+    var mnt = tn.getMonth() /*note that months are 0-indexed*/
+    if (dtw === 0 | dtw === 6)
+        return [];
+    
+    var dow = day_names[dtw-1]
     var final_result = []
     result.forEach((r, ix) => {
-        var is_today = false
+        var is_today = false;
         switch (dow) {
             case 'm':
-                if (r.m == 1) {
+                if (r.m === 1) {
                     is_today = true
-                    break
                 }
+                break;
             case 't':
-                if (r.t == 1) {
+                if (r.t === 1) {
                     is_today = true
-                    break
                 }
+                break;
             case 'w':
-                if (r.w == 1) {
+                if (r.w === 1) {
                     is_today = true
-                    break
                 }
+                break;
             case 'tr':
-                if (r.tr == 1) {
+                if (r.tr === 1) {
                     is_today = true
-                    break
                 }
+                break;
             case 'f':
-                if (r.f == 1) {
+                if (r.f === 1) {
                     is_today = true
-                    break
                 }
+                break;
         }
         if (is_today) {
-            var time_parts = r.time_of_day.split(":")
-            var mins_left = (new Date(yr, mnt, dt, time_parts[0], time_parts[1], time_parts[2]) - new Date())/60000
+            var time_parts = r.time_of_day.split(":").map((ss) => parseInt(ss))
+            /* note that js uses UTC by default which is 4 hours ahead of ET */
+            /* again, note that months are 0-indexed */
+            var mins_left = (new Date(yr, mnt, dt, time_parts[0], time_parts[1], time_parts[2]) - tn)/60000
             final_result[ix] = [r, mins_left]
+        } else {
+            final_result[ix] = false
         }
     })
-    return final_result.filter( Boolean ) // list of [result, mins_left]
+    var output = final_result.filter( is_valid_course )
+    return output// list of [result, mins_left]
     
         //case expression to match the day of the week
         //check times as well
@@ -69,10 +85,10 @@ router.get("/", cors(corsOptions), (req, res, next) => {
     if (vnum)
     {
         connection.query('select * from enrollment e, course_section cs, course c ' +
-                     'where e.section_no = cs.section_no and e.course_no and c.course_no = cs.course_no '+
+                     'where e.section_no = cs.section_no and e.course_no = cs.course_no and c.course_no = cs.course_no '+
                      'and vnum = ?', [vnum], (e,r,f) => {
                         if (!e) {
-                            frs = time_left(r)
+                            var frs = time_left(r)
                             res.json(frs)
                         }
                         else {
